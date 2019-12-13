7897e9769688b2d441e19b11daa824722a43c093
Julia Wakaba <jmugure95@gmail.com>
2019-12-13 22:57:31 +0300

    updated setings

---
+++ bash
@@ -0,0 +1,2 @@
+export SECRET_KEY = "c602f0def89cb349f01525f481a17d034be95bf696dd5110"
+export DEBUG_VALUE = "True"
--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$*1k7u#1)jvv+e28fwqcz^my^km3#yoptn#roj95=ee&tg4k02'
-
+# SECRET_KEY = '$*1k7u#1)jvv+e28fwqcz^my^km3#yoptn#roj95=ee&tg4k02'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['everything-blog.herokuapp.com']
 
 
 # Application definition
